7f715ee1c5e52b7060aadf18ab54770b674ce80c
rasahil <sahilrangari31@gmail.com>
2023-04-13 18:10:58 +0530

    react1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Axiom Techguru <code></code> 
+        </p>
+        <p>To Explore React JS</p>
+
+      <h1>My Name is :-Sahil</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
